8c4530a8e517572fb136d1dd791853f5aac1b24a
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-01-25 10:43:13 +0000

    Implemented API functionality

--- src/script.js
+++ src/script.js
@@ -1,1 +1,42 @@
 import './style.css';
+let leaderboard = document.querySelector('.leaderboard');
+const FORM = document.querySelector('form');
+const NAME = FORM.querySelector('input');
+const SCORE = FORM.querySelector('input[type="number"]');
+
+(() => {
+  FORM.addEventListener('submit', (e) => {
+    e.preventDefault();
+    fetch(
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hu7RDeMDKj2AivBi1yhx/scores',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user: NAME.value,
+          score: SCORE.value,
+        }),
+      }
+    );
+  });
+})();
+
+const refreshLeaderboard = async () => {
+  let response = await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/hu7RDeMDKj2AivBi1yhx/scores'
+  );
+  let scoreText = await response.text();
+  let score = JSON.parse(scoreText);
+  score.result.forEach((player) => {
+    leaderboard.innerHTML += `<tr>
+      <td>${player.user}: ${player.score}</td>
+    </tr>`;
+  });
+};
+
+document.getElementById('refresh').addEventListener('click', () => {
+  leaderboard.innerHTML = '';
+  refreshLeaderboard();
+});
